dfd701fa0287aa5d75d7b7b85661a17a0ca3a92f
decard <mail@decard.space>
2025-09-15 04:43:54 +0800

    Refactor table component and schema for dynamic columns

--- src/ai/shema.ts
+++ src/ai/shema.ts
@@ -1,15 +1,31 @@
 import { z } from 'zod';
 
-export const rowSchema = z.object({
-  title: z.string().describe('Наименование позиции/товара'),
-  amount: z.string().describe('Количество'),
+export const columnSchema = z.object({
+  accessorKey: z.string().describe('Название ключа в данных, например title'),
+  header: z.string().describe('Заголовок для столбца таблицы'),
+  width: z
+    .optional(z.number().min(50))
+    .describe(
+      'Ширина столбца. Нужно устанавливать только когда ширину можно предсказать',
+    ),
 });
+
+export const rowSchema = z
+  .looseObject({})
+  .describe(
+    'Структура объекта строки должна строго соответсвовать заданным accessorKey в столбцах',
+  );
 
 export const messageSchema = z.object({
   text: z
     .string()
     .describe(
-      'Сообщение пользователю. Не используй двоеточие, т.к. сообщения и таблица разделены',
+      'Сообщение пользователю. Не используй двоеточие, т.к. сообщения и таблица разделены. Таблица находится справа.',
+    ),
+  tableColumns: z
+    .optional(z.array(columnSchema))
+    .describe(
+      'Не создавай колонок, когда для них нет данных и сообщи об этом пользователю.',
     ),
   tableRows: z.optional(z.array(rowSchema)),
 });
